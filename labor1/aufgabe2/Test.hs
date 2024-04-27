fd295eaa93fd52f1e73d5d9f4a9e259bb3672700
Elias Gerster <74102959+pedeEli@users.noreply.github.com>
2024-04-27 14:09:45 +0200

    removed unnecessary statment

--- labor1/aufgabe2/Test.hs
+++ labor1/aufgabe2/Test.hs
@@ -1,10 +1,8 @@
 {-# LANGUAGE TemplateHaskell #-}
-{-# OPTIONS_GHC -ddump-splices #-}
 module Test where
 
 import Printf
 
 
 
-
-result = $(printf "Mein Name ist %s und ich bin %d Jahre alt")
+result = $(printf "Mein Name ist %s und ich bin %d Jahre alt") "Elias" 22
