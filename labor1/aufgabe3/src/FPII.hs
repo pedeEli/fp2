61b9479c4faf19ed676076309b04965c13b6e272
Elias Gerster <74102959+pedeEli@users.noreply.github.com>
2024-04-27 17:54:03 +0200

    added implelentations for sort functions

--- labor1/aufgabe3/src/FPII.hs
+++ labor1/aufgabe3/src/FPII.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-@ LIQUID "--no-termination" @-}
 
 module FPII where
@@ -13,16 +14,56 @@ testListSuc = Cons 1 (Cons 2 (Cons 3 Emp))
 testListFail :: SortedList Int
 testListFail = Cons 1 (Cons 3 (Cons 10 Emp))
 
+insert :: (Ord a) => a -> SortedList a -> SortedList a
+insert x Emp = Cons x Emp
+insert x (Cons y ys)
+    | x < y    = Cons x (Cons y ys)
+    | x == y   = insert y ys
+    | otherwise = Cons y (insert x ys)
+
 -- implement insertion sort with explicit recursion
-insertionSort :: Ord a => [a] -> SortedList a
-insertionSort = undefined
+insertionSort :: (Ord a) => [a] -> SortedList a
+insertionSort [] = Emp
+insertionSort (x:xs) = insert x (insertionSort xs)
 
 -- use a suitable higher order function for the same algorithm
 insertionSort' :: Ord a => [a] -> SortedList a
-insertionSort' = undefined
+insertionSort' = foldr insert Emp
 
-quickSort :: Ord a => [a] -> SortedList a
-quickSort = undefined
+
+quickSort :: (Ord a) => [a] -> SortedList a
+quickSort [] = Emp
+quickSort (x:xs) = append x lessers greaters
+  where
+    lessers  = quickSort [y | y <- xs, y < x ]
+    greaters = quickSort [z | z <- xs, z > x]
+
+
+{-@ append :: x:a -> SortedList {lesser:a | lesser < x} -> SortedList {greater:a | greater > x} -> SortedList a @-}
+append :: a -> SortedList a -> SortedList a -> SortedList a
+append z Emp       ys = Cons z ys
+append z (Cons x xs) ys = Cons x $ append z xs ys
+
+
+
+
+merge :: (Ord a) => SortedList a -> SortedList a -> SortedList a
+merge xs  Emp = xs
+merge Emp ys  = ys
+merge (Cons x xs) (Cons y ys)
+  | x < y     = Cons x $ merge xs (Cons y ys)
+  | x > y     = Cons y $ merge (Cons x xs) ys
+  | otherwise = Cons x $ merge xs ys
+merge _ _     = Emp
+
+
+split          :: [a] -> ([a], [a])
+split (x:y:zs) = (x:xs, y:ys)
+  where (xs, ys)   = split zs
+split xs       = (xs, [])
 
 mergeSort :: Ord a => [a] -> SortedList a
-mergeSort = undefined
+mergeSort [] = Emp
+mergeSort [x] = Cons x Emp
+mergeSort xs = merge (mergeSort ys) (mergeSort zs)
+  where (ys, zs) = split xs
